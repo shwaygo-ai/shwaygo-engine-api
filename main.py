6394563d6e46850a0b1956c027092aac705bd3a5
shwaygo-ai <shwaygoai@gmail.com>
2026-06-22 09:40:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import os
 from zenrows import ZenRowsClient
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
-# هذا السطر سيقرأ المفتاح الذي وضعناه في Render تلقائياً
 client = ZenRowsClient(os.getenv("ZENROWS_API_KEY"))
 
 @app.post("/scrape")
-async def scrape_product(url: str):
-    # إعدادات متقدمة لتجاوز الحظر
+async def scrape_product(data: dict = Body(...)):
+    url = data.get("url")
     params = {
         "js_render": "true",
         "antibot": "true",
         "premium_proxy": "true"
     }
-    
-    # تنفيذ الطلب للموقع
     response = client.get(url, params)
-    
-    # إرجاع المحتوى الذي سحبه ZenRows
     return {"content": response.text}
